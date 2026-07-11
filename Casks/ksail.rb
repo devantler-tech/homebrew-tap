aed88f3cb3955022eeb728ebf509ac4841f6055a
devantler <26203420+devantler@users.noreply.github.com>
2026-07-11 17:58:11 +0000

    style: autocorrect Casks (brew style --fix)

--- Casks/ksail.rb
+++ Casks/ksail.rb
@@ -6,7 +6,7 @@ cask "ksail" do
     on_arm do
       sha256 "2829374be6ed72215a0423977666eb61bd64c0d269cfb0020e1c2adef8eb9c1c"
       url "https://github.com/devantler-tech/ksail/releases/download/v#{version}/ksail_#{version}_darwin_arm64.tar.gz",
-        verified: "github.com/devantler-tech/ksail/"
+          verified: "github.com/devantler-tech/ksail/"
     end
   end
 
@@ -14,12 +14,12 @@ cask "ksail" do
     on_intel do
       sha256 "fdc92567032856e27a3bf21f6537e90512cb2cb0996d43b29a5e50cceac8c06e"
       url "https://github.com/devantler-tech/ksail/releases/download/v#{version}/ksail_#{version}_linux_amd64.tar.gz",
-        verified: "github.com/devantler-tech/ksail/"
+          verified: "github.com/devantler-tech/ksail/"
     end
     on_arm do
       sha256 "970d7a287746d9da1c19f0afac45db417ce594210c346602580845d7c3599cdf"
       url "https://github.com/devantler-tech/ksail/releases/download/v#{version}/ksail_#{version}_linux_arm64.tar.gz",
-        verified: "github.com/devantler-tech/ksail/"
+          verified: "github.com/devantler-tech/ksail/"
     end
   end
 
@@ -38,5 +38,4 @@ cask "ksail" do
   end
 
   # No zap stanza required
-
 end
